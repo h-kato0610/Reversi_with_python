dfa5b2085c907029c1007ba75fe3ae38418eb858
h-kato0610 <h.kato0610@gmail.com>
2022-06-14 13:51:08 +0900

    fix 'self' arg to GameDefinitions class and return None to raise NotImplementedError

--- src/game.py
+++ src/game.py
@@ -7,10 +7,10 @@ class GameDefinitions():
     TODO: 外だし
           設定ファイル化
     """
-    def display_size():
+    def display_size(self):
         return (400, 400)
 
-    def display_caption():
+    def display_caption(self):
         return 'Reversi'
 
 class Game():
@@ -18,29 +18,29 @@ class Game():
         definitions = GameDefinitions()
         pygame.init()
         pygame.screen = \
-            pygame.display.set_mode(definitions.display_size)
-        pygame.display.set_caption(definitions.captions)
+            pygame.display.set_mode(definitions.display_size())
+        pygame.display.set_caption(definitions.display_caption())
 
     def __is_winner(self):
         # TODO implemented yet
-        return None
+        raise NotImplementedError
 
     def board(self):
         # TODO implemented yet
-        return None
+        raise NotImplementedError
 
     def score(self):
         # TODO implemented yet
-        return None
+        raise NotImplementedError
 
     def stone(self):
-        # TODO implemented yetする
-        return None
+        # TODO implemented yet
+        raise NotImplementedError
 
     def player(self):
         # TODO implemented yet
-        return None
+        raise NotImplementedError
 
     def enemy(self):
         # TODO implemented yet
-        return None
+        raise NotImplementedError
