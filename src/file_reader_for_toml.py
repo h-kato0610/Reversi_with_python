7d25e4972b2bc39a6ae0a529b055f85c94de601a
h-kato0610 <h.kato0610@gmail.com>
2022-06-21 13:42:23 +0900

    add file_reader class

---
+++ src/file_reader_for_toml.py
@@ -0,0 +1,20 @@
+import toml
+
+from file_reader import FileReader
+
+class FileReaderForToml(FileReader):
+    def set_file_path(self, file_path):
+        self._file_path = file_path
+
+    def load_file(self):
+        """Tomlファイルを読み込む"""
+        try:
+            with open(self._file_path, encoding='utf8') as f:
+                toml_file = toml.load(f)
+                logger.debug(f'read: {self._file_path}')
+                self._contents = toml_file
+        except FileNotFoundError:
+            raise FileNotFoundError(error_value)
+
+    def get_contents(self):
+        return self._contents
