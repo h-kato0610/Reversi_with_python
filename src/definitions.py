cf10cea944ced298c8632cc4a9c58c14ac688957
h-kato0610 <h.kato0610@gmail.com>
2022-06-22 11:16:38 +0900

    definitions.py

---
+++ src/definitions.py
@@ -0,0 +1,13 @@
+import pathlib
+
+from file_reader_for_toml import FileReaderForToml as fr_toml
+
+class Definitions():
+    def __init__(self, definition_filepath):
+        self.definitioin_file = fr_toml()
+        self.definitioin_file.set_file_path(pathlib.Path(definition_filepath))
+        self.definitioin_file.load_file()
+
+    def get_game_definitions():
+        # Implemented yet
+        return None
