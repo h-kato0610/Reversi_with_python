6281c4918ff8b321a1800cf48157c9db1aaf9980
h-kato0610 <h.kato0610@gmail.com>
2022-06-28 10:39:01 +0000

    update definition

--- src/definitions.py
+++ src/definitions.py
@@ -1,16 +1,17 @@
-import pathlib
-
 from file_reader_for_toml import FileReaderForToml as fr_toml
 
 class Definitions():
     def __init__(self, definition_file_path):
-        self.definitioin_file = fr_toml()
-        self.definitioin_file.set_file_path(pathlib.Path(definition_file_path))
-        self.definitioin_file.load_file()
+        tmp = fr_toml()
+        tmp.set_file_path(definition_file_path)
+        tmp.load_file()
+        self.definition_file = tmp.get_contents()
 
-    def get_game_definitions():
-        if display_size:
-            width = int(self.definitioin_file['Game']['display_size']['width'])
-            height = int(self.definitioin_file['Game']['display_size']['height'])
-            return 
-        return self.definitioin_file['Game']
+    def get_caption(self):
+        caption = self.definitioin_file['game']['caption'])
+        return caption
+
+    def get_display_size(self):
+        width = int(self.definitioin_file['game']['display_size']['width'])
+        height = int(self.definitioin_file['game']['display_size']['height'])
+        return (width, height)
