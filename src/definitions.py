0f02f050f1a1bbc74cf61a8d953afcf8a425cdd0
h-kato0610 <h.kato0610@gmail.com>
2022-06-28 13:56:21 +0900

    working now

--- src/definitions.py
+++ src/definitions.py
@@ -3,10 +3,14 @@ import pathlib
 from file_reader_for_toml import FileReaderForToml as fr_toml
 
 class Definitions():
-    def __init__(self, definition_filepath):
+    def __init__(self, definition_file_path):
         self.definitioin_file = fr_toml()
-        self.definitioin_file.set_file_path(pathlib.Path(definition_filepath))
+        self.definitioin_file.set_file_path(pathlib.Path(definition_file_path))
         self.definitioin_file.load_file()
 
     def get_game_definitions():
+        if display_size:
+            width = int(self.definitioin_file['Game']['display_size']['width'])
+            height = int(self.definitioin_file['Game']['display_size']['height'])
+            return 
         return self.definitioin_file['Game']
