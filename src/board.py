e1f8c108338e8c638891b80e248822595ca47f5f
h-kato0610 <h-kato0610.gmail.com>
2022-07-05 15:24:18 +0900

    add board.py

---
+++ src/board.py
@@ -0,0 +1,30 @@
+import pygame
+
+from pygame.locals import *
+
+# TODO to deffile
+WIDTH = 100
+HEIGHT = 100
+COLOR = (0, 255, 0, 0)
+
+class Board:
+    def __init__(self):
+        self.__squares = []
+        self.x = None
+        self.y = None
+        self.white_or_black = None
+        
+    def __initialize_board(self):
+        raise NotImplementedError
+
+    def create_board(self, pygame, screen):
+        raise NotImplementedError
+
+    def update_board(self):
+        raise NotImplementedError
+
+    def draw_board(self, pygame, screen, r):
+        raise NotImplementedError
+
+    def put(self):
+        raise NotImplementedError
