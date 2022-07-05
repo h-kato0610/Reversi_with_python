00ef20f41adcfe620b1726c4212ce7cd5645b70c
h-kato0610 <h-kato0610.gmail.com>
2022-07-05 15:24:35 +0900

    add definition to square num

--- src/definitions.py
+++ src/definitions.py
@@ -28,3 +28,7 @@ class Definitions:
     def get_display_time_wait(self):
         time = int(self.definition_file['game']['display_time_wait'])
         return time
+
+    def get_all_square(self):
+        all_square = int(self.definition_file['game']['all_square'])
+        return all_square
