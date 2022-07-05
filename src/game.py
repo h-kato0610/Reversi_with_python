7868397e791358e8b4f08a591460d37dd2fde8bc
h-kato0610 <h-kato0610.gmail.com>
2022-07-05 23:38:42 +0900

    update mouse event

--- src/game.py
+++ src/game.py
@@ -39,13 +39,21 @@ class Game:
         for board in boards:
             [i.draw_square(pygame, self.screen, square_color) for i in board]
 
+        square_width = self.definitions.get_square_width()
+        square_height = self.definitions.get_square_height()
         while(True):
             # ミリ秒での更新間隔。
             pygame.time.wait(self.definitions.get_display_time_wait())
             pygame.display.update()
-
             # 終了処理
             for event in pygame.event.get():
+                if event.type == MOUSEBUTTONDOWN and event.button == 1:
+                    x, y = event.pos
+                    click_x = int(x // square_width)
+                    click_y = int(y // square_height)
+
+                    is_black=True
+                    boards[click_x][click_y].put(pygame, self.screen, is_black)
                 if event.type == QUIT:
                     self.__end()
                 if event.type == KEYDOWN:
